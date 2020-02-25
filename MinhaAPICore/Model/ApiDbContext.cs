4ba0068de8c46ea20b4a723bc20fcdf88cce1cbe
unknown <fpsantos86@hotmail.com>
2020-02-24 22:31:51 -0300

    Criando minha primeira API REST -> Processo rápido - CRUD

---
+++ MinhaAPICore/Model/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaAPICore.Model
+{
+	public class ApiDbContext : DbContext
+	{
+		public ApiDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Fornecedor> Fornecedores { get; set; }
+
+	}
+}
